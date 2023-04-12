896b041d6a1339eef025bdd0bd1bbb5b37a99fed
LuisOT25 <delatorreluis81@gmail.com>
2023-04-12 12:19:38 -0600

    diseño del component galeria-cursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GaleriaCursosComponent } from './galeria-cursos/galeria-cursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:"galeria",
+  component:GaleriaCursosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
