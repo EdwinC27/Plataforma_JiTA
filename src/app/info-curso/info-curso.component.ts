05a904d46c77ebba1eaa506a20eff57af810d542
LuisOT25 <delatorreluis81@gmail.com>
2023-04-12 18:16:33 -0600

    diseño del component info-curso

--- src/app/info-curso/info-curso.component.ts
+++ src/app/info-curso/info-curso.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-info-curso',
   templateUrl: './info-curso.component.html',
   styleUrls: ['./info-curso.component.css']
+
 })
 export class InfoCursoComponent {
-
+  
+  url:string=''
 }
