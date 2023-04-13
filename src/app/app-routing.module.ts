4a693d74844eaeb5bf216dcd12dd8d92d06eb5be
AlejandroDuenas <alejandroduenas72@gmail.com>
2023-04-12 18:54:42 -0600

    se agrego diseño del componente iniciar-sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GaleriaCursosComponent } from './galeria-cursos/galeria-cursos.component';
 import { InfoCursoComponent } from './info-curso/info-curso.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { TableroCursosComponent } from './tablero-cursos/tablero-cursos.component';
 
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path:"info",
     component:InfoCursoComponent
+  },
+  {
+    path: "iniciar_sesion",
+    component: IniciarSesionComponent
   }
 ];
 
